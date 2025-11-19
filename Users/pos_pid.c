b406fe1a15612b6f83efdb194484c8dfddde8570
Jiang Yi <2241712023@qq.com>
2025-11-19 13:23:10 +0800

    结构体的建立及pid计算函数

--- Users/pos_pid.c
+++ Users/pos_pid.c
@@ -1,1 +1,24 @@
-//111111
+#include"pos_pid.h"
+
+void Pos_PID_Init(float kp, float ki, float kd,float target,float current,float output_max,float integral_max,Pos_PID *pid){
+    pid->kp = kp;
+    pid->ki = ki;
+    pid->kd = kd;
+    pid->integral = 0;
+    pid->current = current;
+    pid->error[0] = 0;
+    pid->error[1] = 0;
+    pid->output = 0;
+    pid->integral_max = integral_max;
+    pid->output_max = output_max;
+}
+void Pos_PID_Calculate(Pos_PID *pid){
+    pid->error[0] = pid->target - pid->current;
+    pid->integral += pid->error[0];
+    if(pid->integral > pid->integral_max) pid->integral = pid->integral_max;
+    else if(pid->integral < -pid->integral_max) pid->integral = -pid->integral_max;
+    pid->output = pid->kp * pid->error[0] + pid->ki * pid->integral + pid->kd * (pid->error[0] - pid->error[1]);
+    pid->error[1] = pid->error[0];
+    if(pid->output > pid->output_max) pid->output = pid->output_max;
+    else if(pid->output <0) pid->output = 0;
+}
